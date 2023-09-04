f78627a06a23acbef9c7eca9cbf6e0350a89dd3e
Abhishek Suman <abhisheksuman413@gmail.com>
2023-09-04 18:48:16 +0530

    60 Day challenge Dya 10

--- 31_Array.kt
+++ 31_Array.kt
@@ -1,8 +1,11 @@
+import java.util.*
+
+
 fun main() {
 
     // Many ways to making/creating Array
 
-    // 1)   arrayOf()
+    // 1) Creating array using arrayOf()
     // Synatx >>>
     // Array bnate hai becouse single datatype ka bhut data store krne ke liye
     // But yha ham datatype defijne nhi kiye hai to alg alg datatype v rakh skte hai
@@ -40,7 +43,7 @@ fun main() {
         println(item)
     }
     
-    // Reassign value useing set function
+    // Reassign value using set function
     
     sit1.set(3,"science") // Set function me 2 parameter hota hai first index and 2nd value
 
@@ -54,7 +57,7 @@ fun main() {
     val a = sit1[3]
     println(a)
     
-    // get value from array useing get function
+    // get value from array using get function
     
     val  a1= sit1.get(3) // // get function me 1 parameter index
     println(a1)
@@ -72,8 +75,6 @@ fun main() {
     // for (item in sit1) { 
     //     println(item)
     // }
-    
-    // Taking user inpute in array 
 
     // Loop Array and their Index
 
@@ -98,7 +99,7 @@ fun main() {
 
     println("Printing size of array sit1 ${sit1.size}")
 
-    // Looping useing array size
+    // Looping using array size
 
     for (i in 0..sit1.size-1){
         println("On index $i value is ${sit1[i]} ")
@@ -108,6 +109,150 @@ fun main() {
     // For each loop
 
     sit1.forEach {i -> println(i) }
+
+
+
+
+
+
+
+
+
+    // 2) Creating array using Array() Constructor
+
+
+    // >>> Array() Constructor me 2 parameter hota hai 1st size and 2nd value which in lambda function
+    // Array() Constructor me hamlog value fanction me dete hai 
+    // Array() Constructor ka use hamlog generally user se inpute lene ke liye krte hai
+
+
+
+    /*
+    
+The Array() constructor in Kotlin provides several benefits and use cases:
+
+Dynamic Array Creation: The Array() constructor allows you to create arrays with a dynamic size at 
+                        runtime. This can be particularly useful when you don't know the size of the 
+                        array in advance and need to determine it based on user input or other runtime 
+                        conditions.
+
+Custom Initialization: You can provide a lambda function to initialize the elements of the array based 
+                       on custom logic. This flexibility allows you to create arrays with elements that 
+                       follow specific patterns, calculations, or user-defined input.
+
+Concise and Readable: The use of the Array() constructor with a lambda function can result in concise 
+                      and readable code, especially when you need to create and initialize arrays in a 
+                      single statement.
+
+Functional Programming: It aligns with functional programming principles, as you can use functional 
+                        constructs like lambdas to define the initialization logic for array elements.
+
+Reduced Boilerplate: It reduces the need for boilerplate code when compared to manually initializing 
+                     arrays element by element in a loop.
+
+Type Safety: Arrays created using the Array() constructor are type-safe, meaning they can only hold 
+             elements of the specified type, helping to catch type-related errors at compile time.
+    */
+
+
+
+    var sit2 = Array(5,{i->
+        when (i) {
+            0 -> "Hello"
+            1 -> "World"
+            2 -> "Kotlin"
+            // 3 -> "Array"
+            else -> "Default"
+        }
+    })
+
+
+
+    for(j in sit2){
+        println(j)
+    }
+
+
+    val scanner = Scanner(System.`in`)
+    println("Enter the size of the array:")
+    val size = scanner.nextInt()
+    
+    // Consume the newline character left by nextInt()
+    scanner.nextLine() // Iss se nextInt ke bad buuffer ko vej rha hai wha se inpute lega
+    
+    // Create an array of the given size and initialize it with user-provided strings
+    val customStrings = Array(size) { index ->
+        println("Enter a string for element $index:")
+        scanner.nextLine()
+    }
+
+    // Display the elements of the custom string array
+    println("You entered the following strings:")
+    for (i in customStrings.indices) {
+        println("printing index $i value ${customStrings[i]}")
+    }
+    
+    /* scanner.close() >>> Using scanner.close() is a good practice when you're done 
+                           using a Scanner object, especially if it's associated with
+                           a system resource like System.in. Closing the Scanner 
+                           object is necessary to release any resources it might be 
+                           holding and to properly clean up*/
+
+
+
+
+
+
+    
+    // 3) Creating array useing Built-in Methods 
+
+    val sit3 = intArrayOf(12,32,54,65,76,76,67)
+    for(i in sit3){
+        println(i)
+    }
+
+    val sit4 = charArrayOf('m','l','d','t','y')
+    for(i in sit4){
+        println(i)
+    }
+    
+    // Iss trh se hi short or long v hota hai 
+    
+    
+    // Copy one array to another array
+    
+    
+    
+    val sit5 = arrayOf<String>("cse","it","ece","ai","ml")
+    for(i in sit5){
+        println("printing array sit5 $i")
+    }
+    
+    val sit6 = sit5.copyOf()
+    
+    for(i in sit6){
+        println("printing array sit6 $i")
+    }
+
+
+
+    // Taking user inpute in array 
+
+   print("Enter no of student ")
+   val num = readLine()!!.toInt()
+   println("Enter student name")
+   var student = Array(num,{readLine()!!})
+   // var student = Array(num){readLine()!!} >>> Uper wale ko ase v likh skte hai 
+
+   for(i in student){
+    println("Name is $i")
+   }
+
+
+ 
+    
+
+
 
 
 }
---
+++ 32_List.kt
@@ -0,0 +1,94 @@
+fun main() {
+
+    /* List :-In Kotlin, a list is a commonly used collection type that represents an
+                 ordered collection of elements. Lists are similar to arrays in other
+                 programming languages, but they offer more flexibility and functionality.
+                 Here are some key characteristics of lists in Kotlin:
+
+    1. **Ordered Collection:** Lists maintain the order of elements in which they were added
+         You can access elements by their index, and the first element is at index 0, the
+         second at index 1, and so on.
+
+    2. **Immutable vs. Mutable Lists:**
+       - **Immutable List:** In Kotlin, you can create an immutable (read-only) list using
+                     the `listOf()` function. Once created, you cannot add, remove, or
+                     modify elements in an immutable list.
+       - **Mutable List:** To create a mutable list that allows you to add, remove, and modify
+                           elements, you can use the `mutableListOf()` function or other
+                           mutable list types like `ArrayList` or `MutableList`.
+    
+    >>> List ka size fix nhi hota hai
+    >>> Element ka value same do alg index pe same v ho skta hai(duplicate allowed hai)
+    >>> List me alg alg data type ke value eak sath store kr skte hai                          */
+
+    
+    
+    
+    
+    // Creating an immutable list
+    val sit = listOf("apple", "banana", "cherry",'r',65)
+
+    // Accessing elements by index 
+    val firstFruit = sit[0] // "apple"
+    println(firstFruit)
+    println(sit[1])
+
+    println(sit) // Iss trh se v pura list print/access ho jyega array me nhi hota hai
+                // Iska value [] me aayega
+               // Bina index ke print/access krna v possible hai pura list
+    
+    // sit.add("orange")
+
+    // Printing by loop
+    for (fruit in sit) {
+        println(fruit)
+    }
+
+    // Get function ke through v print/access kr skte hai
+    println(sit.get(2))
+    
+    // Creating a mutable list
+    // Mutable list me v specific data type ka bna skte hai 
+    val sit1 = mutableListOf("apple", "banana", "cherry")
+    
+    // Adding elements to a mutable list
+    sit1.add(2,"orange")
+    
+    // Removing an element
+    sit1.remove("banana")
+    
+    // Modifying an element
+    sit1[0] = "grape"
+    
+    
+    // Printing by loop
+    for (fruit in sit1) {
+        println(fruit)
+    }
+
+    // Printing Size
+    println("Size of list sit :- ${sit.size}")
+
+    // Printing index
+
+    for(i in sit.indices){
+        println("Printing sit index no :- $i")
+    }
+
+
+    // Printing by for each loop 
+    
+    sit.forEach {i -> println("Printing by for each loop :- $i") }
+
+
+
+    // Specific data type list syntax 
+    val sit2 = listOf<String>("apple", "banana", "cherry")
+    for(i in sit2){
+        println("Printing sit2 elment :- $i")
+    }
+
+    // Other function are same as array
+
+
+}
